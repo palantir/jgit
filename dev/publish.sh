0b39a7c8c791e99c9144ee1037f3b507857c94a9
Jiahui Jiang <jjiang@palantir.com>
2018-06-28 16:41:34 -0400

    fix deploy

--- dev/publish.sh
+++ dev/publish.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 FWDIR="$(cd "`dirname "${BASH_SOURCE[0]}"`"; pwd)"
 
-source "$FWDIR/publish_functions.sh"
+source "$FWDIR/publish-functions.sh"
 
 publish_artifacts | tee -a "/tmp/publish_artifacts.log"
